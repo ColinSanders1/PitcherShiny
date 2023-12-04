c31e9c9d26e4aabb2bca7ae218c7d75f930f5fa8
ColinSanders1 <94136154+ColinSanders1@users.noreply.github.com>
2023-12-04 16:20:21 -0500

    Update app.R

--- app.R
+++ app.R
@@ -2,12 +2,11 @@ library(baseballr)
 library(shiny)
 library(dplyr)
 library(plotly)
-library(ggplot2)
 library(readxl)
 library(openxlsx)
 
 tryCatch({
-  data <- read.xlsx("~/IDtable.xlsx")
+  data <- read.xlsx("IDtable.xlsx")
 }, error = function(e) {
   print(e)
 })
@@ -22,19 +21,20 @@ player_id <- function(last_name, first_name, data) {
 }
 
 ui <- fluidPage(
-  titlePanel("Pitcher Report"),
+  titlePanel("MLB Pitcher Report"),
   sidebarLayout(
     sidebarPanel(
-      textInput("last_name", "Last Name", ""),
       textInput("first_name", "First Name", ""),
+      textInput("last_name", "Last Name", ""),
       actionButton("search_btn", "Retrieve Data")
     ),
     mainPanel(
       plotlyOutput("pitch_movement_plot"), 
       plotlyOutput("pitch_location_plot"),
-      tableOutput("Avg_Stats"),
-      plotlyOutput("Pitch_perc_by_ball_count"),
-      plotlyOutput("Pitch_perc_by_strike_count")
+      tableOutput("Avg_Stats1"),
+      tableOutput("Avg_Stats2"),
+      plotlyOutput("Pitch_perc_combined_str"),
+      plotlyOutput("Pitch_perc_combined_ball")
     )
   )
 )
@@ -46,9 +46,8 @@ server <- function(input, output, session) {
     player_id_result <- player_id(last_name, first_name, data)
     
     if (player_id_result == "player not found") {
-      return(NULL)  # Player not found, return NULL
+      return(NULL)  
     } else {
-      # Fetch pitch data for the selected player based on their ID
       dat <- scrape_statcast_savant(
         start_date = '2023-03-25',
         end_date = Sys.Date(),
@@ -60,7 +59,6 @@ server <- function(input, output, session) {
   })
   
   observeEvent(input$search_btn, {
-    # Update the choices in the selectInput based on the retrieved player data
     updateSelectInput(session, "selected_name", choices = unique(player_data()$player_name))
   })
   
@@ -68,24 +66,39 @@ server <- function(input, output, session) {
     selected_name <- input$selected_name
     selected_name
   })
-  
-  output$Avg_Stats <- renderTable({
-    player_selected_data <- player_data()
+  output$Avg_Stats1 <- renderTable({
+    player_selected_data1 <- player_data()
     
-    if (!is.null(player_selected_data)) {
-      # Retrieve data for the selected player and create the player_avg_data table
-      player_avg_data <- player_selected_data %>%
+    if (!is.null(player_selected_data1)) {
+      player_avg_data1 <- player_selected_data1 %>%
         group_by(player_name, pitch_name) %>%
         summarize(
           Average_SpinRate = mean(release_spin_rate, na.rm = TRUE),
           Average_Velocity = mean(release_speed, na.rm = TRUE),
           Average_Extension = mean(release_extension, na.rm = TRUE),
           Average_ReleaseHeight = mean(release_pos_y, na.rm = TRUE),
-          XWOBA = mean(estimated_woba_using_speedangle, na.rm = TRUE),
-          Average_Exit_Velocity = mean(launch_speed, na.rm = TRUE),
-          Average_Launch_Angle = mean(launch_angle, na.rm = TRUE)
         )
-      player_avg_data
+      player_avg_data1
+    }
+  })
+  
+  output$Avg_Stats2 <- renderTable({
+    player_selected_data2 <- player_data()
+    
+    vyf = -sqrt(player_selected_data2$vy0^2 - (2 * player_selected_data2$ay * (50 - 17/12)))
+    t = (vyf - player_selected_data2$vy0) / player_selected_data2$ay
+    vzf = player_selected_data2$vz0 + (player_selected_data2$az * t)
+    player_selected_data2$vaa = -atan(vzf / vyf) * (180 / pi)
+    
+    if (!is.null(player_selected_data2)) {
+      player_avg_data2 <- player_selected_data2 %>%
+        group_by(player_name, pitch_name) %>%
+        summarize(Average_Vertical_Approach = mean(vaa, na.rm = TRUE),
+                  XWOBA = mean(estimated_woba_using_speedangle, na.rm = TRUE),
+                  Average_Exit_Velocity = mean(launch_speed, na.rm = TRUE),
+                  Average_Launch_Angle = mean(launch_angle, na.rm = TRUE)
+        )
+      player_avg_data2
     }
   })
   
@@ -106,7 +119,6 @@ server <- function(input, output, session) {
         group_by(pitch_name) %>%
         summarise(Pitch_Count = n())
       
-      # Calculate the percentage for each pitch type
       pitch_counts_data <- pitch_counts_data %>%
         mutate(Percentage = (Pitch_Count / sum(Pitch_Count)) * 100)
       
@@ -114,55 +126,65 @@ server <- function(input, output, session) {
     }
   })
   
-  # Create a summary table to count pitch types by strikes
-  pitch_counts_strikes <- reactive({
+  pitch_percentages_by_strikes <- reactive({
     player_selected_data <- player_data()
     
     if (!is.null(player_selected_data)) {
       pitch_counts_data <- player_selected_data %>%
-        group_by(pitch_name, strikes) %>%
+        group_by(pitch_name, strikes, stand) %>%
         summarise(Pitch_Count = n())
       
-      # Calculate the percentage for each pitch type by strike count
       pitch_counts_data <- pitch_counts_data %>%
-        group_by(strikes) %>%
+        group_by(strikes, stand) %>%
         mutate(Percentage = (Pitch_Count / sum(Pitch_Count)) * 100)
       
       return(pitch_counts_data)
     }
   })
   
-  # Create a summary table to count pitch types by balls
-  pitch_counts_balls <- reactive({
+  pitch_percentages_by_balls <- reactive({
     player_selected_data <- player_data()
     
     if (!is.null(player_selected_data)) {
       pitch_counts_data <- player_selected_data %>%
-        group_by(pitch_name, balls) %>%
+        group_by(pitch_name, balls, stand) %>%
         summarise(Pitch_Count = n())
       
-      # Calculate the percentage for each pitch type by ball count
       pitch_counts_data <- pitch_counts_data %>%
-        group_by(balls) %>%
+        group_by(balls, stand) %>%
         mutate(Percentage = (Pitch_Count / sum(Pitch_Count)) * 100)
       
       return(pitch_counts_data)
     }
   })
   
-  # Create a scatter plot for pitch movement with labels for pitch counts
+  pitch_percentages_by_stand <- reactive({
+    player_selected_data <- player_data()
+    
+    if (!is.null(player_selected_data)) {
+      pitch_counts_data <- player_selected_data %>%
+        group_by(pitch_name, stand) %>%
+        summarise(Pitch_Count = n())
+      
+      pitch_percentages_data <- pitch_counts_data %>%
+        group_by(stand) %>%
+        mutate(Percentage = (Pitch_Count / sum(Pitch_Count)) * 100)
+      
+      return(pitch_percentages_data)
+    }
+  })
+  
+  
   output$pitch_movement_plot <- renderPlotly({
     player_selected_data <- player_data()
     
     if (!is.null(player_selected_data)) {
       pitch_movement_data <- player_selected_data
       
-      # Create a scatter plot using plotly with color based on pitch_name and labels for pitch counts
       plot <- plot_ly(data = pitch_movement_data, x = ~pfx_x, y = ~pfx_z, type = 'scatter',
                       mode = 'markers', text = ~paste(pitch_name, " (", pitch_counts()$Pitch_Count[match(pitch_name, pitch_counts()$pitch_name)], ")", sep = ""), 
                       color = ~pitch_name, colors = 'Set1', marker = list(size = 5))
       
-      # Customize the plot layout
       plot <- plot %>% layout(
         title = "Pitch Movement by Type",
         xaxis = list(title = "Horizontal Movement (pfx_x)"),
@@ -199,36 +221,44 @@ server <- function(input, output, session) {
     }
   })
   
-  output$Pitch_perc_by_strike_count <- renderPlotly({
-    bar_chart <- plot_ly(data = pitch_counts_strikes(), x = ~factor(strikes), y = ~Percentage,
-                         type = 'bar', color = ~pitch_name, text = ~paste(pitch_name, ": ", Percentage, "%"),
-                         marker = list(line = list(width = 2))) %>%
-      layout(
-        title = "Pitch Type Percentages by Strike Count",
-        xaxis = list(title = "Strikes"),
-        yaxis = list(title = "Percentage (%)"),
-        barmode = 'stack'
-      )
+  output$Pitch_perc_combined_str <- renderPlotly({
+    pitch_percentages_strikes <- pitch_percentages_by_strikes()
     
-    return(bar_chart)
+    if (!is.null(pitch_percentages_strikes)) {
+      plot <- ggplot(data = pitch_percentages_strikes, aes(x = factor(strikes), y = Percentage, fill = pitch_name)) +
+        geom_bar(stat = "identity", position = "dodge", width = 0.8) +
+        labs(title = "Pitch Type Percentages by Strike Count",
+             x = "Strikes",
+             y = "Percentage (%)",
+             fill = "Pitch Name") +
+        theme_minimal() +
+        facet_wrap(~stand, ncol = 2) 
+      
+      plot <- ggplotly(plot)
+      
+      return(plot)
+    }
   })
   
-  # Create a bar chart for pitch type percentages by ball count
-  output$Pitch_perc_by_ball_count <- renderPlotly({
-    bar_chart <- plot_ly(data = pitch_counts_balls(), x = ~factor(balls), y = ~Percentage,
-                         type = 'bar', color = ~pitch_name, text = ~paste(pitch_name, ": ", Percentage, "%"),
-                         marker = list(line = list(width = 2))) %>%
-      layout(
-        title = "Pitch Type Percentages by Ball Count",
-        xaxis = list(title = "Balls"),
-        yaxis = list(title = "Percentage (%)"),
-        barmode = 'stack'
-      )
+  output$Pitch_perc_combined_ball <- renderPlotly({
+    pitch_percentages_balls <- pitch_percentages_by_balls()
     
-    return(bar_chart)
+    if (!is.null(pitch_percentages_balls)) {
+      plot <- ggplot(data = pitch_percentages_balls, aes(x = factor(balls), y = Percentage, fill = pitch_name)) +
+        geom_bar(stat = "identity", position = "dodge", width = 0.8) +
+        labs(title = "Pitch Type Percentages by Ball Count",
+             x = "Balls",
+             y = "Percentage (%)",
+             fill = "Pitch Name") +
+        theme_minimal() +
+        facet_wrap(~stand, ncol = 2)  
+      
+      plot <- ggplotly(plot)
+      
+      return(plot)
+    }
   })
 }
 
 # Run the application
 shinyApp(ui = ui, server = server)
-
